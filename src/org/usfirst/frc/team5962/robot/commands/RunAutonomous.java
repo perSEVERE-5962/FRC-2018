15308752eb29f1d8f06e9997ea479f426037737d
Yash Patel <patelshay8@gmail.com>
2018-01-22 19:21:15 -0500

    PrintingVirtues

--- src/org/usfirst/frc/team5962/robot/commands/RunAutonomous.java
+++ src/org/usfirst/frc/team5962/robot/commands/RunAutonomous.java
@@ -1,16 +1,22 @@
 package org.usfirst.frc.team5962.robot.commands;
 
+import org.usfirst.frc.team5962.robot.subsystems.Autonomous;
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class RunAutonomous extends Command {
 
 	private boolean isFinished = false;
+	private Autonomous autonomousSubsystem = new Autonomous();
 
 	protected void initialize() {
-
+		autonomousSubsystem.init();
 	}
 
 	protected void execute() {
+		//autonomousSubsystem.printHeavenlyVirtues();
+		//autonomousSubsystem.determineGoodPerson();
 
 	}
 
