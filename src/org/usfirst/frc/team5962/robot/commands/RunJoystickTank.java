383a13f917aeb5921728290bd6126bf91195df24
Unknown <aidangalgano@gmail.com>
2018-02-11 11:16:18 -0500

    Throttle

--- src/org/usfirst/frc/team5962/robot/commands/RunJoystickTank.java
+++ src/org/usfirst/frc/team5962/robot/commands/RunJoystickTank.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team5962.robot.commands;
 
 import org.usfirst.frc.team5962.robot.Robot;
+import org.usfirst.frc.team5962.robot.subsystems.Joystick;
 
 import edu.wpi.first.wpilibj.command.Command;
 
@@ -18,7 +19,10 @@ public class RunJoystickTank extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	
+  
     	Robot.drive.joystickTank();
+    	Joystick.throttle();
     }
 
     // Make this return true when this Command no longer needs to run execute()
