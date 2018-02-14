f0c011daf5c97b7e1a3f5da913dedcf4b6134d06
Yash Patel <patelshay8@gmail.com>
2018-02-14 18:31:14 -0500

    Auto stash before merge of "YashSalamander" and "origin/Kevin"

---
+++ src/org/usfirst/frc/team5962/robot/subsystems/PIDDropIntakeSource.java
@@ -0,0 +1,27 @@
+package org.usfirst.frc.team5962.robot.subsystems;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.PIDSourceType;
+import edu.wpi.first.wpilibj.PIDSource;
+import org.usfirst.frc.team5962.robot.Robot;
+import org.usfirst.frc.team5962.robot.RobotMap;
+
+public class PIDDropIntakeSource {
+
+	public void setPIDSourceType(PIDSourceType pidSource) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public PIDSourceType getPIDSourceType() {
+		// TODO Auto-generated method stub
+		return PIDSourceType.kDisplacement;
+	}
+
+	@Override
+	public double pidGet() {
+		// TODO Auto-generated method stub
+		return (Robot.encoder.getDistance());
+	}
+}
---
+++ src/org/usfirst/frc/team5962/robot/subsystems/PIDDropIntakeSubsystems.java
@@ -0,0 +1,34 @@
+package org.usfirst.frc.team5962.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.PIDSubsystem;
+
+
+import org.usfirst.frc.team5962.robot.RobotMap;
+import org.usfirst.frc.team5962.robot.Robot;
+
+public class PIDDropIntakeSubsystems {
+
+	public PIDDriveSubsystems(double p, double i, double d) {
+		super(p, i, d);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected double returnPIDInput() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	protected void usePIDOutput(double output) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
