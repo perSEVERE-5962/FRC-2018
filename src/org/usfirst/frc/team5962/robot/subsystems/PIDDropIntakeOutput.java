8559416d35d765474bbab20998147390ba5705ae
Yash Patel <patelshay8@gmail.com>
2018-02-14 18:31:26 -0500

    test

--- src/org/usfirst/frc/team5962/robot/subsystems/PIDDropIntakeOutput.java
+++ src/org/usfirst/frc/team5962/robot/subsystems/PIDDropIntakeOutput.java
@@ -1,5 +1,12 @@
 package org.usfirst.frc.team5962.robot.subsystems;
 
+
+import org.usfirst.frc.team5962.robot.RobotMap;
+import org.usfirst.frc.team5962.robot.Robot;
+
 public class PIDDropIntakeOutput {
 
+	public void pidWrite(double output) {
+		
+	}
 }
